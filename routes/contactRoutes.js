e7be6e23b76d8f9c9ea4399b2e45b61ef9a4eb8f
Yassine Laaguidi <aitlaaguidiyassine@gmail.com>
2024-08-18 16:14:20 +0200

    adding mongoDB and collection

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -4,16 +4,13 @@ const {getContacts,
     createContact,
     getContact,
     updateContact,
-    deleteContact} = require("../controllers/contactController");
+    deleteContact
+    } = require("../controllers/contactController");
 
-router.route('/').get(getContacts);
+router.route('/').get(getContacts).post(createContact);
 
-router.route('/').post(createContact);
 
-router.route('/:id').get(getContact);
+router.route('/:id').get(getContact).put(updateContact).delete(deleteContact);
 
-router.route('/:id').put(updateContact);
-
-router.route('/:id').delete(deleteContact);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,32 @@
 const express = require('express');
+const connectDb = require("./config/dbConnection");
 const dotenv = require('dotenv').config()
+const errorHandler = require("./middleware/errorHandler");
+const mongoose = require("mongoose");
 
+connectDb();
 const app = express();
 
 
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use(errorHandler);
+
+/*const uri = 'mongodb+srv://admin:admin@contacts-nodejs.phsmc.mongodb.net/?retryWrites=true&w=majority&appName=Contacts-nodejs'
+*/
+/*async function connect() {
+    try {
+      await mongoose.connect(uri);
+      console.log("Connected to MongoDB");
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+  connect();
+  */
 
 app.listen(port, () => {
     console.log(`Server running on port ${5000}`)
