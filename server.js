c1a78f5b5d88aef70b6f2a37f08f26a9e22c0c86
Yassine Laaguidi <aitlaaguidiyassine@gmail.com>
2024-08-31 13:04:33 +0200

    Project finished

--- server.js
+++ server.js
@@ -1,35 +1,18 @@
-const express = require('express');
+const express = require("express");
 const connectDb = require("./config/dbConnection");
-const dotenv = require('dotenv').config()
 const errorHandler = require("./middleware/errorHandler");
-const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
 
 connectDb();
 const app = express();
-
 
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
 app.use(errorHandler);
 
-/*const uri = 'mongodb+srv://admin:admin@contacts-nodejs.phsmc.mongodb.net/?retryWrites=true&w=majority&appName=Contacts-nodejs'
-*/
-/*async function connect() {
-    try {
-      await mongoose.connect(uri);
-      console.log("Connected to MongoDB");
-    } catch (error) {
-      console.error(error);
-    }
-  }
-  
-  connect();
-  */
-
 app.listen(port, () => {
-    console.log(`Server running on port ${5000}`)
-})
-
-
+    console.log(`Server running on port ${port}`);
+});
